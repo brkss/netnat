cfd7108743971f28dcbdf9b1b39874a2f4716bc0
Brahim Berkasse <berkassebrahim@gmail.com>
2021-12-06 17:59:46 +0100

    🚀 accept request !

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,13 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
 const websocket_1 = require("websocket");
+const uuid_1 = require("uuid");
 const PORT = 4000;
 (async () => {
+    const _room = {};
+    console.log("uuid => ", (0, uuid_1.v4)());
     const serv = http_1.default.createServer();
     serv.listen(PORT);
     console.log(`🚀 server runing at http://localhost:${PORT} !`);
     const ws = new websocket_1.server({
         httpServer: serv,
+    });
+    ws.on("request", (request) => {
+        const userID = (0, uuid_1.v4)();
+        console.log(`[${new Date()}] recieved new reauest from ${request.origin}`);
+        const connection = request.accept("", request.origin);
+        _room[userID] = connection;
+        console.log(`user joined the room ${userID}`);
     });
 })();
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,27 @@
 import http from "http";
 import { server } from "websocket";
+import { v4 as uuidv4 } from "uuid";
 
 const PORT: number = 4000;
 
 (async () => {
+  const _room: any = {};
+  console.log("uuid => ", uuidv4());
   const serv = http.createServer();
   serv.listen(PORT);
   console.log(`🚀 server runing at http://localhost:${PORT} !`);
 
   const ws = new server({
     httpServer: serv,
+  });
+
+  ws.on("request", (request) => {
+    const userID = uuidv4();
+    console.log(`[${new Date()}] recieved new reauest from ${request.origin}`);
+
+    // accept connection !
+    const connection = request.accept("", request.origin);
+    _room[userID] = connection;
+    console.log(`user joined the room ${userID}`);
   });
 })();
