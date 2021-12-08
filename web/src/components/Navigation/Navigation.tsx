70672ca1ab650292385fde8547816122949c5e1b
Brahim Berkasse <berkassebrahim@gmail.com>
2021-12-08 19:01:48 +0100

    🖍  Navigation !

--- web/src/components/Navigation/Navigation.tsx
+++ web/src/components/Navigation/Navigation.tsx
@@ -1,15 +1,39 @@
 import React from "react";
-import { Box, Grid, GridItem, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  GridItem,
+  Heading,
+  Badge,
+  Avatar,
+  Text,
+} from "@chakra-ui/react";
 
 export const Navigation: React.FC = () => {
   return (
-    <Box>
+    <Box bg={"#1e191a"} p={"7px"} w={"100%"}>
       <Grid templateColumns="repeat(12, 1fr)">
-        <GridItem>
-          <Heading fontSize={"12px"}>netnat</Heading>
+        <GridItem colSpan={6}>
+          <Heading fontSize={"17px"}>netnat</Heading>
         </GridItem>
-        <GridItem>
-          <Box>H</Box>
+        <GridItem colSpan={6} alignItems={"end"} textAlign={"right"}>
+          <Badge
+            bg={"#403134"}
+            color={"white"}
+            rounded={"50px"}
+            p={"4px"}
+            pr={"8px"}
+          >
+            <Avatar src="https://bit.ly/broken-link" size={"xs"} />
+            <Text
+              display={"inline-block"}
+              mt={"5px"}
+              ml={"3px"}
+              fontSize={"10px"}
+            >
+              avocado-youate
+            </Text>
+          </Badge>
         </GridItem>
       </Grid>
     </Box>
